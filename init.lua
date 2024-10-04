2da5488dfe9365dcd12abbf2ae35e692c1e5a259
LordIdra <percy.baker.0@gmail.com>
2024-10-04 01:36:33 +0100

    stuff

--- init.lua
+++ init.lua
@@ -46,6 +46,7 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   end,
 })
 
+vim.keymap.set('n', '<leader>e', '<cmd>NERDTreeToggle<CR>', { desc = 'Toggle [E]xplorer' })
 vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
 vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
@@ -120,6 +121,8 @@ require('lualine').setup {
 }
 
 require('trouble').setup({})
+
+require('nvim-web-devicons').setup({})
 
 vim.cmd [[ 
   sign define DiagnosticSignError text=  linehl= texthl=DiagnosticSignError numhl= 
